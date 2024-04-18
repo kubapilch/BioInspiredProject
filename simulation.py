ece97dda5d968d360de31c8f8ce95753d75bf735
Jakub Pilch <jakub@hamso.dk>
2024-04-18 12:50:51 +0200

    Drones with plotting repulsion vector
    Co-authored-by: Richard Žember <Klaserko@users.noreply.github.com>

--- simulation.py
+++ simulation.py
@@ -25,20 +25,38 @@ ax = plt.axes(xlim=(0, ARENA_SIDE_LENGTH), ylim=(0, ARENA_SIDE_LENGTH))
 
 (drone_points,) = ax.plot([], [], "bo", lw=0, label="Drone")
 (sheep_points,) = ax.plot([], [], "ro", lw=0, label="Sheep")
-direction_lines = [ax.plot([], [], "k-", lw=1)[0] for _ in range(NUMBER_OF_ROBOTS)]  # Lines indicating direction
-drone_labels = [ax.text(0, 0, "", ha='center', va='center') for _ in range(NUMBER_OF_ROBOTS)]  # Labels for drones
+direction_lines = [
+    ax.plot([], [], "k-", lw=1)[0] for _ in range(NUMBER_OF_ROBOTS)
+]  # Lines indicating direction
+drone_labels = [
+    ax.text(0, 0, "", ha="center", va="center") for _ in range(NUMBER_OF_ROBOTS)
+]  # Labels for drones
+repulsion_vectors = [
+    ax.plot([], [], "g-", lw=1)[0] for _ in range(NUMBER_OF_ROBOTS)
+]  # Repulsion vectors
 
 ax.legend()
+
 
 def init():
     drone_points.set_data([], [])
     sheep_points.set_data([], [])
     return drone_points, sheep_points
 
+
 def plot_visibility_squares():
-     for drone in world.drones:
+    for drone in world.drones:
         x, y = drone.absolute_pos
-        ax.add_patch(plt.Rectangle((x - CELL_SIZE/2, y - CELL_SIZE/2), CELL_SIZE, CELL_SIZE, color='b', alpha=0.1))
+        ax.add_patch(
+            plt.Rectangle(
+                (x - CELL_SIZE / 2, y - CELL_SIZE / 2),
+                CELL_SIZE,
+                CELL_SIZE,
+                color="b",
+                alpha=0.1,
+            )
+        )
+
 
 def plot_visited_cells():
     plotted_cells = []
@@ -47,11 +65,18 @@ def plot_visited_cells():
         for x in range(world.cell_number):
             for y in range(world.cell_number):
                 if drone.visited_cells[x][y] and not (x, y) in plotted_cells:
-                    ax.add_patch(plt.Rectangle((x * CELL_SIZE, y * CELL_SIZE), CELL_SIZE, CELL_SIZE, color='r', alpha=0.1))
+                    ax.add_patch(
+                        plt.Rectangle(
+                            (x * CELL_SIZE, y * CELL_SIZE),
+                            CELL_SIZE,
+                            CELL_SIZE,
+                            color="r",
+                            alpha=0.1,
+                        )
+                    )
 
                     plotted_cells.append((x, y))
 
-    
 
 def plot_direction_lines():
     for i, drone in enumerate(world.drones):
@@ -60,18 +85,32 @@ def plot_direction_lines():
             target_x = drone.target_cell[0] * CELL_SIZE + CELL_SIZE / 2
             target_y = drone.target_cell[1] * CELL_SIZE + CELL_SIZE / 2
             # Update line between drone and center of target cell
-            direction_lines[i].set_data([drone.absolute_pos[0], target_x], [drone.absolute_pos[1], target_y])
+            direction_lines[i].set_data(
+                [drone.absolute_pos[0], target_x], [drone.absolute_pos[1], target_y]
+            )
+
 
 def plot_drone_labels():
     for i, drone in enumerate(world.drones):
         # Update drone label
         drone_labels[i].set_text(f"Sheep: {drone.number_of_sheeps_visible}")
-        drone_labels[i].set_position((drone.absolute_pos[0], drone.absolute_pos[1] + CELL_SIZE))
+        drone_labels[i].set_position(
+            (drone.absolute_pos[0], drone.absolute_pos[1] + CELL_SIZE)
+        )
+
+
+def plot_repulsion_vectors():
+    for i, drone in enumerate(world.drones):
+        repulsion_vectors[i].set_data(
+            [drone.absolute_pos[0], drone.absolute_pos[0] + drone.repulsion[0]],
+            [drone.absolute_pos[1], drone.absolute_pos[1] + drone.repulsion[1]],
+        )
+
 
 def animate(i):
-    if(i % 10 != 0):
+    if i % 10 != 0:
         return drone_points, sheep_points
-    
+
     # Update the world
     world.update()
 
@@ -85,10 +124,11 @@ def animate(i):
     for overlay in ax.patches:
         overlay.remove()
 
-    plot_visibility_squares()  # Plot new circles
-    plot_visited_cells() # Plot visited cells¨
-    plot_direction_lines()  # Plot direction lines
-    plot_drone_labels()  # Plot drone labels
+    plot_visibility_squares()
+    plot_visited_cells()
+    plot_direction_lines()
+    plot_drone_labels()
+    plot_repulsion_vectors()
 
     # Update sheep positions on the plot
     sheep_positions_x = [sheep.x for sheep in world.sheep]
@@ -102,6 +142,6 @@ def animate(i):
 
 anim = FuncAnimation(fig, animate, init_func=init, frames=STEPS, interval=1, blit=True)
 
-plt.show()
+# plt.show()
 videowriter = animation.FFMpegWriter(fps=10)
 anim.save("output.mp4", writer=videowriter)
